59b61b3d85192da2b95648fa0e1787f51cd06079
&rew Peterson <drewcpete@gmail.com>
2019-09-06 16:26:39 -0700

    adds some css styling

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-
 $(document).ready(function(){
   $(".age").submit(function(event){
     event.preventDefault();
