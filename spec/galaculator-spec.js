50f31e8b4916596ab8b0c6521fcb31943d86a48a
&rew Peterson <drewcpete@gmail.com>
2019-09-06 11:52:54 -0700

    add text output for life expectancy functions

--- spec/galaculator-spec.js
+++ spec/galaculator-spec.js
@@ -2,28 +2,33 @@ import { Age } from "./../src/galaculator.js"
 
 
 describe('Age', function(){
-  let testAge = 5;
+  let testAge = 1;
+  let testExp = 100;
   it('should test whether the age on Mercury is .24 earth years', function(){
-    let testAgeObj = new Age(testAge)
-    expect(testAgeObj.getMer()).toEqual(.24)
-  })
+    let testAgeObj = new Age(testAge);
+    expect(testAgeObj.getMer()).toEqual(testAge*.24);
+  });
 
   it('should test whether the age on Venus is .62 earth years', function(){
-    let testAgeObj = new Age(testAge)
-    expect(testAgeObj.getVen()).toEqual(.62)
-  })
-
-
+    let testAgeObj = new Age(testAge);
+    expect(testAgeObj.getVen()).toEqual(testAge*.62);
+  });
 
   it('should test whether the age on Mars is 1.88 earth years', function(){
-    let testAgeObj = new Age(testAge)
-    expect(testAgeObj.getMar()).toEqual(1.88)
-  })
-
-
+    let testAgeObj = new Age(testAge);
+    expect(testAgeObj.getMar()).toEqual(testAge*1.88);
+  });
 
   it('should test whether the age on Jupiter is 11.86 earth years', function(){
-    let testAgeObj = new Age(testAge)
-    expect(testAgeObj.getJup()).toEqual(11.86)
+    let testAgeObj = new Age(testAge);
+    expect(testAgeObj.getJup()).toEqual(testAge*11.86);
+  });
+
+  it('should check to see if the person has lived beyond their life expectancy', function(){
+
   })
+
+  it('should test how many years the person has left to live', function(){
+
+  });  
 });
--- src/galaculator.js
+++ src/galaculator.js
@@ -1,66 +1,70 @@
 export class Age {
-  constructor(earAge) {
+  constructor(earAge, earExp) {
     this.earAge = earAge;
-  }
+    this.earExp = earExp;
+  };
 
   getJup(){
     const jup = 11.86;
     let jupAge = this.earAge * jup;
     return jupAge;
-  }
+  };
 
   getMer(){
     const mer = .24;
     let merAge = this.earAge * mer;
     return merAge;
-  }
+  };
 
   getVen(){
     const ven = .62;
     let venAge = this.earAge * ven;
     return venAge;
-  }
+  };
 
   getMar(){
     const mar = 1.88;
     let marAge = this.earAge * mar;
     return marAge;
+  };
+
+  getJupLife() {
+    let life = this.earExp - this.earAge;
+    let jupLife = life*11.86;
+    if (jupLife < 0) {
+      return "You're " + jupLife*-1 + " years over your expiration date!";
+    } else {
+      return "You have " + jupLife + " years left before your death.";
+    }
   }
-}
 
+  getMarLife() {
+    let life = this.earExp - this.earAge;
+    let marLife = life*1.88;
+    if (marLife < 0) {
+      return "You're " + marLife*-1 + " years over your expiration date!";
+    } else {
+      return "You have " + marLife + " years left before your death.";
+    }
+  };
 
+  getVenLife() {
+    let life = this.earExp - this.earAge;
+    let venLife = life*.62;
+    if (venLife < 0) {
+      return "You're " + venLife*-1 + " years over your expiration date!";
+    } else {
+      return "You have " + venLife + " years left before your death.";
+    }
+  };
 
-
-// this.venAge = this.earAge * .62;
-// this.venExpect = this.earExpect * .62;
-// this.venLeft = this.venExpect - this.venAge;
-
-// this.marAge = this.earAge * 1.88;
-// this.marExpect = this.earExpect * 1.88;
-// this.marLeft = this.marExpect - this.marAge;
-
-// this.merAge = this.earAge * .24;
-// this.merExpect = this.earExpect * .24;
-// this.merLeft = this.merExpect - this.merAge;
-
-
-// , merAge, venAge, marAge, jupAge, earExpect, jupExpect, marExpect, venExpect, merExpect, merLeft, venLeft, marLeft, jupLeft
-
-
-
-
-
-// this.earAge = earAge;
-// this.merAge = 0;
-// this.venAge = 0;
-// this.marAge = 0;
-// this.jupAge = 0;
-// this.merLeft = 0;
-// this.venLeft = 0;
-// this.marLeft = 0;
-// this.jupLeft = 0;
-// this.earExpect = earExpect;
-// this.jupExpect = 0;
-// this.marExpect = 0;
-// this.venExpect = 0;
-// this.merExpect = 0;
+  getMerLife() {
+    let life = this.earExp - this.earAge;
+    let merLife = life*.24
+    if (merLife < 0) {
+      return "You're " + merLife*-1 + " years over your expiration date!";
+    } else {
+      return "You have " + merLife + " years left before your death.";
+    }
+  }
+};
